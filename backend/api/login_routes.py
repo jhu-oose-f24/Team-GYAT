26005c2daa94c86fe8d6cfed5b0e1a439a5626dc
Andrew Myers <amyers42@jh.edu>
2024-11-09 16:48:40 -0500

    fix?

--- backend/api/login_routes.py
+++ backend/api/login_routes.py
@@ -1,17 +1,16 @@
-from flask import Blueprint, redirect, session
-from models.login import service_provider
+from flask import Blueprint, redirect, session, url_for
 
 login_bp = Blueprint('login_bp', __name__)
 
 @login_bp.route('/jhu/login', methods=['GET'])
 def login():
-    return redirect(service_provider.login_url())
+    return redirect(url_for('flask_saml2_sp.login'))
 
 @login_bp.route('/jhu/logout')
 def logout():
     session.clear()
-    return redirect(service_provider.logout_url())
+    return redirect(url_for('flask_saml2_sp.logout'))
 
 @login_bp.route('/jhu/metadata', methods=['GET'])
 def metadata():
-    return service_provider.create_metadata_response()
+    return redirect(url_for('flask_saml2_sp.metadata'))
