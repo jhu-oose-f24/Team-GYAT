e9c4fbc87385c9e323c397b457810f8645d1e043
Andrew Myers <amyers42@jh.edu>
2024-11-12 15:15:55 -0500

    Fix missing login and other views in JHUServiceProvider

--- backend/models/login.py
+++ backend/models/login.py
@@ -2,7 +2,7 @@ from flask_saml2.sp import ServiceProvider
 from flask_saml2.utils import certificate_from_file, private_key_from_file
 from config import Config
 from custom_views import CustomAssertionConsumer
-from flask import Blueprint
+from flask import Blueprint, current_app
 
 class JHUServiceProvider(ServiceProvider):
     def get_sp_entity_id(self):
@@ -21,16 +21,17 @@ class JHUServiceProvider(ServiceProvider):
         return certificate_from_file("certs/cert.pem")
 
     def get_assertion_consumer_service_view(self):
-        print("Custom get_assertion_consumer_service_view() called")
+        current_app.logger.info("Custom get_assertion_consumer_service_view() called")
         return CustomAssertionConsumer.as_view('acs', self)
 
     def create_blueprint(self):
+        # Creating the main SAML blueprint
         bp = Blueprint('jhu_saml2_sp', __name__)
-        bp.route('/login/', methods=['GET'], endpoint='login')(self.login)  # Inherited from ServiceProvider
-        bp.route('/logout/', methods=['GET'], endpoint='logout')(self.logout)  # Inherited from ServiceProvider
+        bp.route('/login/', methods=['GET'], endpoint='login')(self.login)
+        bp.route('/logout/', methods=['GET'], endpoint='logout')(self.logout)
         bp.route('/acs/', methods=['POST'], endpoint='acs')(self.get_assertion_consumer_service_view())
-        bp.route('/sls/', methods=['GET', 'POST'], endpoint='sls')(self.single_logout)  # Inherited
-        bp.route('/metadata/', methods=['GET'], endpoint='metadata')(self.metadata)  # Inherited
+        bp.route('/sls/', methods=['GET', 'POST'], endpoint='sls')(self.single_logout)
+        bp.route('/metadata/', methods=['GET'], endpoint='metadata')(self.metadata)
         return bp
 
 service_provider = JHUServiceProvider()
