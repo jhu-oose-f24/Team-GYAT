e854028dcada636504e5b60adaa5d0cd6fe9f85b
Andrew Myers <amyers42@jh.edu>
2024-11-13 10:13:06 -0500

    Fix SAML configuration and IdP handler

--- backend/config.py
+++ backend/config.py
@@ -1,15 +1,19 @@
 import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'team_gyat')
 
     # SAML Configurations
     SAML2_SP = {
         'entity_id': 'https://task-market-7ba3283496a7.herokuapp.com',
-        'acs_url': 'https://task-market-7ba3283496a7.herokuapp.com/sso/acs/',
-        'sls_url': 'https://task-market-7ba3283496a7.herokuapp.com/sso/sls/',
-        'certificate': 'certs/sp_certificate.pem',
-        'private_key': 'certs/sp_private_key.pem',
+        'acs_url': 'https://task-market-7ba3283496a7.herokuapp.com/saml/acs/',
+        'sls_url': 'https://task-market-7ba3283496a7.herokuapp.com/saml/sls/',
+        'certificate': os.path.join(BASE_DIR, 'certs', 'sp_certificate.pem'),
+        'private_key': os.path.join(BASE_DIR, 'certs', 'sp_private_key.pem'),
     }
+
     # Identity Providers configuration
     SAML2_IDENTITY_PROVIDERS = [
         {
@@ -18,8 +22,7 @@ class Config:
                 "entity_id": "https://login.jh.edu/idp/shibboleth",
                 "sso_url": "https://login.jh.edu/idp/profile/SAML2/Redirect/SSO",
                 "slo_url": "https://login.jh.edu/cgi-bin/logoff.pl",
-                'x509cert': open('certs/idp_certificate.pem').read(),
-
+                "certificate": os.path.join(BASE_DIR, 'certs', 'idp_certificate.pem'),
             },
         }
     ]
--- backend/models/login.py
+++ backend/models/login.py
@@ -2,13 +2,12 @@ from flask_saml2.sp import ServiceProvider
 from flask_saml2.utils import certificate_from_file, private_key_from_file
 from flask import redirect, url_for, flash
 from flask_login import login_user
-from models.User import User
+from models.user import User
 from models import db
 from config import Config
+import os
 
 class MyServiceProvider(ServiceProvider):
-
-    blueprint_name = 'saml'
     def get_sp_entity_id(self):
         return Config.SAML2_SP['entity_id']
 
@@ -19,14 +18,16 @@ class MyServiceProvider(ServiceProvider):
         return Config.SAML2_SP['sls_url']
 
     def get_sp_private_key(self):
-        return private_key_from_file(Config.SAML2_SP['private_key'])
+        private_key_path = Config.SAML2_SP['private_key']
+        return private_key_from_file(private_key_path)
 
     def get_sp_certificate(self):
-        return certificate_from_file(Config.SAML2_SP['certificate'])
+        certificate_path = Config.SAML2_SP['certificate']
+        return certificate_from_file(certificate_path)
 
     def get_idp_configs(self):
         return Config.SAML2_IDENTITY_PROVIDERS
-
+    
     def create_blueprint(self):
         bp = super().create_blueprint()
         bp.name = 'saml'
