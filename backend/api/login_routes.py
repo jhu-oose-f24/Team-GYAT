219f1e0da46b4f90bd33f26d889947722a69caee
Andrew Myers <amyers42@jh.edu>
2024-11-13 09:26:13 -0500

    added logging

--- backend/api/login_routes.py
+++ backend/api/login_routes.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, redirect, url_for
 from models.login import sp
-
+import logging
 login_bp = Blueprint('login_bp', __name__)
+logging.basicConfig(level=logging.DEBUG)
 
 @login_bp.route('/login')
 def login():
+    logging.debug("Redirecting to SAML login")
     # Redirect to the SAML login using the correct endpoint
     return redirect(url_for('flask_saml2_sp.login'))
 
