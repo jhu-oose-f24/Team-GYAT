c1b89c10fa0ccca15f22bfdd2f8590e3d53a2c44
Andrew Myers <amyers42@jh.edu>
2024-11-18 15:31:11 -0500

    fixing app circular dependencies

--- backend/app.py
+++ backend/app.py
@@ -40,6 +40,9 @@ def create_app():
 
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+    print(app.url_map)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
