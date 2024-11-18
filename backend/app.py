cad606d945c1fd6be3fa97cf490775ca59cc1b7b
Andrew Myers <amyers42@jh.edu>
2024-11-18 15:17:14 -0500

    fixed

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import pymysql
 pymysql.install_as_MySQLdb()
-
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -22,10 +22,11 @@ def seed_tags():
     db.session.commit()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static')
     CORS(app)
-    # db configuration
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:team-gyat@127.0.0.1/task-market-db'
+
+    # Database configuration
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('JAWSDB_URL') or os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # associate db with app
